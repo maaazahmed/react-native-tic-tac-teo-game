5384ea37c4bb60540777339baa273e47ee7859cd
maaazahmed <maazahmed2k16@gmail.com>
2018-10-06 15:44:14 +0500

    Updated

--- src/Component/Game/index.js
+++ src/Component/Game/index.js
@@ -80,7 +80,7 @@ export default class Game extends Component {
                 // isAlert: true,
                 gameWinner: 1
             })
-            alert("X Winner")
+            // alert("X Winner")
             this.initializeGame()
             this.winnerAnimatedViw()
         }
@@ -89,7 +89,7 @@ export default class Game extends Component {
                 // isAlert: true,
                 gameWinner: -1
             })
-            alert("O Winner")
+            // alert("O Winner")
             this.winnerAnimatedViw()
             this.initializeGame()
         }
@@ -176,25 +176,24 @@ export default class Game extends Component {
 
     newGame = () => {
         this.initializeGame()
-        // Animated.sequence([
-        Animated.timing(this.state.winnerAlert, {
-            toValue: 1,
-            duration: 500,
-        }).start()
-        // ])
-
-        // Animated.parallel([
-        Animated.timing(this.state.winnerAlertHeight, {
-            toValue: 1,
-            duration: 400,
-            easing: Easing.elastic(),
-        }),
+        Animated.parallel([
+            Animated.timing(this.state.winnerAlert, {
+                toValue: 0,
+                duration: 200,
+            }).start()
+        ])
+        Animated.parallel([
+            Animated.timing(this.state.winnerAlertHeight, {
+                toValue: 0,
+                duration: 400,
+                easing: Easing.elastic(),
+            }),
             Animated.timing(this.state.winnerAlertborderRadius, {
-                toValue: 1,
+                toValue: 0,
                 duration: 500,
                 easing: Easing.bounce,
             }).start()
-        // ]).start()
+        ]).start()
         this.setState({ isAlert: false })
     }
 
@@ -210,18 +209,14 @@ export default class Game extends Component {
             inputRange: [0, 1],
             outputRange: [0, 1]
         })
-
         const height = this.state.winnerAlertHeight.interpolate({
             inputRange: [0, 0.5, 1],
-            outputRange: [100, 200, 300]
+            outputRange: ["0%", "45%", "50%"]
         })
-
         const width = this.state.winnerAlertHeight.interpolate({
             inputRange: [0, 0.5, 1],
-            outputRange: [100, 200, 300]
+            outputRange: ["0%", "45%", "80%"]
         })
-
-
         const borderRadius = this.state.winnerAlertborderRadius.interpolate({
             inputRange: [0, 1],
             outputRange: [50, 2]
@@ -231,95 +226,84 @@ export default class Game extends Component {
 
         return (
             <View style={styles.container}>
-                {(this.state.isAlert) ?
-                    <Animated.View style={{
-                        height,
-                        backgroundColor: "#0da192",
-                        width, zIndex: 1,
-                        opacity,
-                        borderRadius,
-                        position: "absolute"
+                <Animated.View style={{
+                    height,
+                    backgroundColor: "#0da192",
+                    width, zIndex: 1,
+                    opacity,
+                    borderRadius,
+                    position: "absolute"
+                }} >
+                    <View style={{
+                        justifyContent: "center",
+                        alignItems: "center",
+                        flex: 1,
                     }} >
-                        <View style={{
-                            justifyContent: "center",
-                            alignItems: "center",
-                            flex: 1,
-                        }} >
-                            {(this.state.gameWinner === 1) ?
-                                <View>
-                                    <View style={{ justifyContent: "center", alignItems: "center" }} >
-                                        <Icon name="close" style={[styles.tilX, { fontSize: 80, color: "#fff" }]} />
-                                        <Text style={{ color: "#545454", fontSize: 50 }} >Winner!</Text>
-                                    </View>
-                                    <TouchableOpacity
-                                        activeOpacity={0.6}
-                                        onPress={() => { this.newGame() }}
-                                        style={{ marginTop: 50, borderColor: "#0da192", borderWidth: 3, padding: 10 }}
-                                    >
-                                        <Text style={{ fontSize: 20, color: "#0da192", fontWeight: "bold" }} >New Game</Text>
-                                    </TouchableOpacity>
+                        {(this.state.gameWinner === 1) ?
+                            <View>
+                                <View style={{ justifyContent: "center", alignItems: "center" }} >
+                                    <Icon name="close" style={[styles.tilX, { fontSize: 80, color: "#fff" }]} />
+                                    <Text style={{ color: "#545454", fontSize: 50 }} >Winner!</Text>
                                 </View>
-                                :
-                                <View>
-                                    <View style={{ justifyContent: "center", alignItems: "center" }} >
-                                        <Icon name="radio-button-off" style={[styles.tilX, { fontSize: 80, color: "#fff" }]} />
-                                        <Text style={{ color: "#545454", fontSize: 50 }} >Winner!</Text>
-                                    </View>
-                                    <TouchableOpacity
-                                        activeOpacity={0.6}
-                                        onPress={() => { this.newGame() }}
-                                        style={{ marginTop: 50, borderColor: "#0da192", borderWidth: 3, padding: 10 }}
-                                    >
-                                        <Text style={{ fontSize: 20, color: "#0da192", fontWeight: "bold" }} >New Game</Text>
-                                    </TouchableOpacity>
-                                </View>}
-
+                            </View>
+                            :
+                            <View>
+                                <View style={{ justifyContent: "center", alignItems: "center" }} >
+                                    <Icon name="radio-button-off" style={[styles.tilX, { fontSize: 80, color: "#fff" }]} />
+                                    <Text style={{ color: "#545454", fontSize: 50 }} >Winner!</Text>
+                                </View>
+                            </View>}
+                        <View style={{ marginTop: "10%" }} >
+                            <TouchableOpacity activeOpacity={0.6} onPress={() => { this.newGame() }} >
+                                <Icon name="refresh" style={[styles.tilX, { fontSize: 80, color: "#fff" }]} />
+                            </TouchableOpacity>
                         </View>
-                    </Animated.View>
-                    :
-                    <View style={{ justifyContent: "center", alignItems: "center" }} >
-                        <View style={{ alignItems: "center", justifyContent: "center" }} >
-                            <View style={{ flexDirection: "row" }} >
-                                <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(0, 0)}
-                                    style={[styles.box, { borderTopWidth: 0, borderLeftWidth: 0, }]} >
-                                    {this.renderIcon(0, 0)}
-                                </TouchableOpacity>
-                                <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(0, 1)} style={[styles.box, { borderTopWidth: 0, }]} >
-                                    {this.renderIcon(0, 1)}
-                                </TouchableOpacity>
-                                <TouchableOpacity onPress={() => this.onTilePress(0, 2)} style={[styles.box, { borderTopWidth: 0, borderRightWidth: 0, }]} >
-                                    {this.renderIcon(0, 2)}
-                                </TouchableOpacity>
-                            </View>
+
+                    </View>
+                </Animated.View>
+                <View style={{ justifyContent: "center", alignItems: "center" }} >
+                    <View style={{ alignItems: "center", justifyContent: "center" }} >
+                        <View style={{ flexDirection: "row" }} >
+                            <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(0, 0)}
+                                style={[styles.box, { borderTopWidth: 0, borderLeftWidth: 0, }]} >
+                                {this.renderIcon(0, 0)}
+                            </TouchableOpacity>
+                            <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(0, 1)} style={[styles.box, { borderTopWidth: 0, }]} >
+                                {this.renderIcon(0, 1)}
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => this.onTilePress(0, 2)} style={[styles.box, { borderTopWidth: 0, borderRightWidth: 0, }]} >
+                                {this.renderIcon(0, 2)}
+                            </TouchableOpacity>
+                        </View>
 
 
-                            <View style={{ flexDirection: "row" }} >
-                                <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(1, 0)} style={[styles.box, { borderLeftWidth: 0, }]} >
-                                    {this.renderIcon(1, 0)}
-                                </TouchableOpacity>
-                                <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(1, 1)} style={[styles.box, {}]} >
-                                    {this.renderIcon(1, 1)}
-                                </TouchableOpacity>
-                                <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(1, 2)} style={[styles.box, { borderRightWidth: 0, }]} >
-                                    {this.renderIcon(1, 2)}
-                                </TouchableOpacity>
-                            </View>
+                        <View style={{ flexDirection: "row" }} >
+                            <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(1, 0)} style={[styles.box, { borderLeftWidth: 0, }]} >
+                                {this.renderIcon(1, 0)}
+                            </TouchableOpacity>
+                            <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(1, 1)} style={[styles.box, {}]} >
+                                {this.renderIcon(1, 1)}
+                            </TouchableOpacity>
+                            <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(1, 2)} style={[styles.box, { borderRightWidth: 0, }]} >
+                                {this.renderIcon(1, 2)}
+                            </TouchableOpacity>
+                        </View>
 
 
-                            <View style={{ flexDirection: "row" }} >
-                                <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(2, 0)} style={[styles.box, { borderLeftWidth: 0, borderBottomWidth: 0 }]} >
-                                    {this.renderIcon(2, 0)}
-                                </TouchableOpacity>
-                                <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(2, 1)} style={[styles.box, { borderBottomWidth: 0 }]} >
-                                    {this.renderIcon(2, 1)}
-                                </TouchableOpacity>
-                                <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(2, 2)} style={[styles.box, { borderBottomWidth: 0, borderRightWidth: 0 }]} >
-                                    {this.renderIcon(2, 2)}
-                                </TouchableOpacity>
-                            </View>
+                        <View style={{ flexDirection: "row" }} >
+                            <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(2, 0)} style={[styles.box, { borderLeftWidth: 0, borderBottomWidth: 0 }]} >
+                                {this.renderIcon(2, 0)}
+                            </TouchableOpacity>
+                            <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(2, 1)} style={[styles.box, { borderBottomWidth: 0 }]} >
+                                {this.renderIcon(2, 1)}
+                            </TouchableOpacity>
+                            <TouchableOpacity activeOpacity={0.6} onPress={() => this.onTilePress(2, 2)} style={[styles.box, { borderBottomWidth: 0, borderRightWidth: 0 }]} >
+                                {this.renderIcon(2, 2)}
+                            </TouchableOpacity>
                         </View>
                     </View>
-                }
+                </View>
+                {/* } */}
                 <TouchableOpacity
                     activeOpacity={0.6}
                     onPress={() => { this.newGame() }}
